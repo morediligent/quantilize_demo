9c941d0a4f4d17d85bb3609deae06ae06ea118a4
ylqfp <ylqfps@gmail.com>
2016-11-03 10:06:37 +0800

    add single date test

--- webserver.py
+++ webserver.py
@@ -8,12 +8,36 @@ from collections import deque
 from flask import *
 import xlwt
 import StringIO
+from collections import defaultdict
+import random
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 #url_for('static', filename='echarts.js')
+fin=open("zhailist.csv", 'r')
+fin.readline()
+
+dic = defaultdict(dict)
+
+for line in fin:
+	#if N>=5:
+	#	break
+	lineu = line.strip().decode("gb18030")
+	#print lineu
+	#s = "startdate=2001-01-01;enddate=2016-09-19;windcode=%s" % lineu
+	#d = w.wset("guaranteedbond",s)
+	lines = lineu.split(",")
+	zhaiid = lines[0]
+	hangye1 = lines[1].strip()
+	hangye2 = lines[2].strip()
+	if hangye1 == "0":
+		continue
+	if hangye2 == "0":
+		continue
+	dic[hangye1][hangye2] = 1
 
 option = {
             "title": {
@@ -34,15 +58,119 @@ option = {
             }]
         }
 
+def testdataGenerator(cur_date="2016-11-01", prev_date=None, level1=None, level2=None):
+	"""
+	生成测试数据
+	"""
+	if prev_date==None and level1==None and level2==None: #某一天所有行业的风险值
+		# generator data sequence
+		lst = []
+		for hangye1 in dic.keys():
+			dic_item = {}
+			shu = round(random.uniform(1,100), 2)
+			#outline = "%s,level1,%s,%.2f" % (dat, hangye1, shu)
+			dic_item[u"日期"] = cur_date
+			dic_item[u"第一行业"] = hangye1
+			dic_item[u"第二行业"] = u"全部"
+			dic_item[u"数值"] = shu
+			lst.append(dic_item)
+			
+		for hangye1 in dic.keys():
+			for hangye2 in dic[hangye1].keys():
+				dic_item = {}
+				shu = round(random.uniform(1,100), 2)
+				#shu1 = "%.2f" % (shu,)
+				#outline = "%s,level2,%s,%.2f,%s" % (dat, hangye2, shu, hangye1)
+				dic_item[u"日期"] = cur_date
+				dic_item[u"第一行业"] = hangye1
+				dic_item[u"第二行业"] = hangye2
+				dic_item[u"数值"] = shu	
+				lst.append(dic_item)
+		outstr = json.dumps(lst, ensure_ascii = False)#.encode("utf-8")
+		return outstr
+	elif prev_date == None and level2 == None and not level1 == None: #某一天某一级行业的风险值
+		lst = []
+		for hangye1 in dic.keys():
+			if not hangye1 == level1:
+				continue
+			dic_item = {}
+			shu = round(random.uniform(1,100), 2)
+			#outline = "%s,level1,%s,%.2f" % (dat, hangye1, shu)
+			dic_item[u"日期"] = cur_date
+			dic_item[u"第一行业"] = hangye1
+			dic_item[u"第二行业"] = u"全部"
+			dic_item[u"数值"] = shu
+			lst.append(dic_item)
+			
+		for hangye1 in dic.keys():
+			if not hangye1 == level1:
+				continue		
+			for hangye2 in dic[hangye1].keys():
+
+				dic_item = {}
+				shu = round(random.uniform(1,100), 2)
+				#shu1 = "%.2f" % (shu,)
+				#outline = "%s,level2,%s,%.2f,%s" % (dat, hangye2, shu, hangye1)
+				dic_item[u"日期"] = cur_date
+				dic_item[u"第一行业"] = hangye1
+				dic_item[u"第二行业"] = hangye2
+				dic_item[u"数值"] = shu	
+				lst.append(dic_item)
+		outstr = json.dumps(lst, ensure_ascii = False)#.encode("utf-8")
+		return outstr	
+	elif prev_date == None and not level2 == None and not level1 == None: #某一天某一级和二级行业的风险值
+		lst = []
+		"""
+		for hangye1 in dic.keys():
+			if not hangye1 == level1:
+				continue
+			dic_item = {}
+			shu = round(random.uniform(1,100), 2)
+			#outline = "%s,level1,%s,%.2f" % (dat, hangye1, shu)
+			dic_item[u"日期"] = cur_date
+			dic_item[u"第一行业"] = hangye1
+			dic_item[u"第二行业"] = u"全部"
+			dic_item[u"数值"] = shu
+			lst.append(dic_item)
+		"""
+			
+		for hangye1 in dic.keys():
+			if not hangye1 == level1:
+				continue		
+			for hangye2 in dic[hangye1].keys():
+				if not hangye2 == level2:
+					continue			
+				dic_item = {}
+				shu = round(random.uniform(1,100), 2)
+				#shu1 = "%.2f" % (shu,)
+				#outline = "%s,level2,%s,%.2f,%s" % (dat, hangye2, shu, hangye1)
+				dic_item[u"日期"] = cur_date
+				dic_item[u"第一行业"] = hangye1
+				dic_item[u"第二行业"] = hangye2
+				dic_item[u"数值"] = shu	
+				lst.append(dic_item)
+		outstr = json.dumps(lst, ensure_ascii = False)#.encode("utf-8")
+		return outstr	
+	else:
+		return "hello world!"
+		#fout.write(json.dumps(dic_item, ensure_ascii=False).encode("utf-8") + ",\n" )	
+
+	
 @app.route('/')
 def index():
-	return render_template('home.html', name="Hahaha")
+	#return render_template('home.html', name="Hahaha")
+	#return testdataGenerator(cur_date = "2016-11-02", level1 = u"房地产")
+	return testdataGenerator(cur_date = "2016-11-02", level1 = u"房地产", level2 = u"房地产开发")
 
-@app.route('/test')
+@app.route('/test', methods=['GET', 'POST'])
 def test():
-	global option
-	lst = [5, 20, 36, 10, 10, 20]
-	return render_template('show.html', mydata = option)
+	#global option
+	#lst = [5, 20, 36, 10, 10, 20]
+	#return render_template('show.html', mydata = option)
+	cur_date = request.form['date']
+	level1 = request.form['level1']
+	level2 = request.form['level2']
+	return testdataGenerator(cur_date = cur_date, level1 = level1, level2 = level2)
 
 
 @app.route('/linebar')
